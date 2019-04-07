6a8816600d70eae94cea8ca0d5a596d7171f5da2
Rambo <Vivek>
2019-04-07 12:12:46 +0530

    knex db setup and migration added with create user

---
+++ knexfile.js
@@ -0,0 +1,8 @@
+module.exports = {
+    client: 'mysql',
+    connection: {
+        user: 'root',
+        password: 'r00t',
+        database: 'node_mysql_database'
+    }
+}
--- store.js
+++ store.js
@@ -1,7 +1,12 @@
+const knex = require('knex')(require('./knexfile'))
+
 module.exports = {
     createUser({ username, password }) {
         let message = `Add user ${username} with password ${password}`;
         console.log(message)
-        return Promise.resolve(message)
+        return knex('user').insert({
+            username,
+            password
+        })
     }
 }
